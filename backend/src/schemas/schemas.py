bf2b2b9a829f1fca1a5cb2c5a4348624154fd16b
Ulvi Nasibli <ulvinasibli@Ulvis-MacBook-Pro.local>
2024-11-21 21:10:27 +0200

    Remove unwanted folders from api branch

--- backend/src/schemas/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class UserRegistration(BaseModel):
-    email: EmailStr
-    password: str
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
--- server/index.js
+++
@@ -1,1 +0,0 @@
-comsole.log("server is running")
